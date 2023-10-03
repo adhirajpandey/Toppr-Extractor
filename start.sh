a39f20d8169ec2dcf8c7ee361dad4b7cfe59a0ff
adhirajpandey <87516052+adhirajpandey@users.noreply.github.com>
2023-10-04 05:25:56 +0530

    add changes from deployed instance

--- start.sh
+++ start.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-source venv/bin/activate
+sleep 10
 
-mkdir DATA
+# Change working directory to /home/ubuntu/Deepgaze-API/
+cd '/home/adhiraj/Desktop/Projects/Toppr Celery'
+
+source venv/bin/activate
 
 # Check if the Flask server process is running
 if ps aux | grep -v grep | grep "python3 app.py" >/dev/null; then
     # Flask server process is running, capture its PID
-    pid=$(ps aux | grep -v grep | grep "python3 app.py" | awk '{print $2}')
+    pid1=$(ps aux | grep -v grep | grep "python3 app.py" | awk '{print $2}')
 
-    echo "PID is: $pid"
+    echo "PID1 is: $pid1"
 
     # Kill the Flask server process
-    sudo kill -9 $pid
+    sudo kill -9 $pid1
 
-    echo "Process killed successfully."
+    echo "Flask Process killed successfully."
+
+    pid2=$(ps aux | grep -v grep | grep "celery -A tasks worker -l info" | awk '{print $2}')
+
+    echo "PID2 is: $pid2"
+
+    # Kill the Celery process
+    sudo kill -9 $pid2
+
+    echo "Celery Process killed successfully."
+
+    pid3=$(ps aux | grep -v grep | grep "1280x1024x24" | awk '{print $2}')
+
+    echo "PID3 is: $pid3"
+
+    # Kill the xvfb process
+    sudo kill -9 $pid3
+
+    echo "xvfb display Process killed successfully."
+
 else
     echo "No process found."
 fi
 
+# Start the Flask server again but with virtual display
 nohup python3 app.py >> flasknohup.out 2>&1 &
+# xvfb-run -a -s "-screen 0 1280x1024x24" nohup python3 app.py >> nohup.out 2>&1 &
 
-nohup celery -A tasks worker -l info >> celerynohup.out 2>&1 &
+xvfb-run -a -s "-screen 0 1280x1024x24" nohup celery -A tasks worker -l info >> celerynohup.out 2>&1 &
 
 
 echo "New Flask Server and Celery Worker started successfully."
